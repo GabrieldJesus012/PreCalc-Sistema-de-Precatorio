111ef4a57d9e82b08d08e650819a625904115ad9
Gabriel de Jesus Silva <gabrieldjesus055@outlook.com>
2025-02-22 08:28:36 -0300

    vb

---
+++ PreCalc/PreCalc/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim xlApp As Object
+        Dim xlWb As Object
+        Dim xlWbName As String
+        Dim xlWbPath As String
+
+        On Error Resume Next
+
+        xlApp = CreateObject("Excel.Application")
+
+        xlWbName = "PreCalc.xlsm"
+        xlWbPath = Application.StartupPath
+
+        Me.Hide()
+
+        xlWb = xlApp.workbooks.open(xlWbPath & "\" & xlWbName)
+
+
+        xlApp = Nothing
+        xlWbName = Nothing
+        xlWbPath = Nothing
+        xlWb = Nothing
+
+        Application.Exit()
+    End Sub
+End Class
